a92caf9a6ebab7061c98135a642e43557960aab5
CloudflareMrX <122194108+CloudflareMrX@users.noreply.github.com>
2024-01-21 22:47:52 +0530

    config.py

--- config.py
+++ config.py
@@ -5,17 +5,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 LOGGER = logging.getLogger(__name__)
 
 class Config(object):
-    APP_ID = 
-    API_HASH = ""
-    BOT_TOKEN = ""
-    BOT_USERNAME = ""
-    OWNER_ID = 
+    APP_ID = 21712687
+    API_HASH = "397ff79e47bafe94c6609f68c5a8e7f5"
+    BOT_TOKEN = "6842307779:AAFZAclKkOA5aILTmvM1oVAMHO4PJtUNmOM
+"
+    BOT_USERNAME = "Nandumrxrips"
+    OWNER_ID = 5960644989
     AUTH_IDS = []
-    QOBUZ_MAIL = ""
+    QOBUZ_MAIL = "paxetin714@ikuromi.com"
     HEROKU_APP_NAME = "xxxx" # dont touch
     HEROKU_API_KEY = "xxxx" # dont touch
-    QOBUZ_PASS = ""
-    QOBUZ_QUAL = 
+    QOBUZ_PASS = "Forqobuz1213"
+    QOBUZ_QUAL = 5
     UPDATE_ALL = True
     LOG_CHANNEL = []
     botStartTime = time.time() # dont touch
